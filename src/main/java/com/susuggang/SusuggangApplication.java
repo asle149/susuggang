dcb6fbcc7cd2e62e250d0eace6430ef35cad5665
asle149 <202085976+asle149@users.noreply.github.com>
2026-07-02 01:04:44 +0900

    SSG-7 chore: Spring Boot 단일 모듈 프로젝트 초기 세팅 (의존성·PostgreSQL 연결)

---
+++ src/main/java/com/susuggang/SusuggangApplication.java
@@ -0,0 +1,13 @@
+package com.susuggang;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SusuggangApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SusuggangApplication.class, args);
+	}
+
+}
